b5aeb7db023269724037c9d87095120861c9301f
ecurran00 <43557818+ecurran00@users.noreply.github.com>
2019-05-06 15:42:16 -0400

    FINAL EDITS
    final edits

--- js/nav.js
+++ js/nav.js
@@ -22,3 +22,25 @@ open.addEventListener("click", function(){
         changeIcon = true;
     }
 });
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".navigation a").on('click', function(event) {
+  
+      // Prevent default anchor click behavior
+      event.preventDefault();
+  
+      // Store hash
+      var hash = this.hash;
+  
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+     
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    });
+  });
